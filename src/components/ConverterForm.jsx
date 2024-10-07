2296172102939b95b2da89e281d3823a73feb908
You218 <rishabhmishra5252@gmail.com>
2024-10-08 01:41:54 +0530

    Initial commit for Vite project

---
+++ src/components/ConverterForm.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import CurrencySelect from "./CurrencySelect";
+
+const ConverterForm = () => {
+    const [fromCurrency, setFromCurrency] = useState("USD");
+    const [toCurrency, setToCurrency] = useState("INR");
+    const [amount, setAmount] = useState(1); // Add amount state
+    const [result, setResult] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSwapCurrencies = () => {
+        setFromCurrency(toCurrency);
+        setToCurrency(fromCurrency);
+    };
+
+    const getExchangeRate = async () => {
+        const API_KEY = "13b48c38228afa1ab16d14fa";
+        const BASE_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}`;
+        const API_URL = `${BASE_URL}/pair/${fromCurrency}/${toCurrency}`;
+
+        if (isLoading) return;
+        setIsLoading(true);
+
+        try {
+            const response = await fetch(API_URL);
+            if (!response.ok) throw Error("Something went wrong!");
+
+            const data = await response.json();
+            const rate = (data.conversion_rate * amount).toFixed(2);
+            setResult(`${amount} ${fromCurrency} = ${rate} ${toCurrency}`);
+        } catch (error) {
+            setResult("Something went wrong!");
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        getExchangeRate();
+    };
+
+    useEffect(() => {
+        getExchangeRate();
+    }, []);
+
+    return (
+        <form className="converter-form" onSubmit={handleFormSubmit}>
+            <div className="form-group">
+                <label className="form-label">Enter Amount</label>
+                <input
+                    type="number"
+                    className="form-input"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                    required
+                />
+            </div>
+
+            <div className="form-group form-currency-group">
+                <div className="form-section">
+                    <label className="form-label">From</label>
+                    <CurrencySelect
+                        selectedCurrency={fromCurrency}
+                        handleCurrency={e => setFromCurrency(e.target.value)}
+                    />
+                </div>
+
+                <div className="swap-icon" onClick={handleSwapCurrencies}>
+                    {/* Your SVG icon here */}
+                </div>
+
+                <div className="form-section">
+                    <label className="form-label">To</label>
+                    <CurrencySelect
+                        selectedCurrency={toCurrency}
+                        handleCurrency={e => setToCurrency(e.target.value)}
+                    />
+                </div>
+            </div>
+
+            <button type="submit" className={`${isLoading ? "loading" : ""} submit-button`}>Get Exchange Rate</button>
+            <p className="exchange-rate-result">
+                {isLoading ? "Getting exchange rate..." : result}
+            </p>
+        </form>
+    );
+};
+
+export default ConverterForm;
